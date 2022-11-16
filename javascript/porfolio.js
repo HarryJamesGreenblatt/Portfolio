dd63a8379c7fac9bfc19e732da98fc36d4588bfa
HarryJamesGreenblatt <HarryJamesGreenblatt@ServiceCentricSolutions.com>
2022-11-15 22:54:17 -0800

    new file:   css/portfolio.css new file:   img/00559-icon-service-Azure-Cloud-Shell.svg new file:   img/10216-icon-service-Azure-Data-Catalog.svg new file:   img/10221-icon-service-Azure-Active-Directory.svg new file:   img/10241-icon-service-Microsoft-Defender-for-Cloud.svg new file:   img/10248-icon-service-Azure-Sentinel.svg new file:   img/SMRC Logo.png new file:   img/as_kid.jpg new file:   img/atlassian.png new file:   img/azure.png new file:   img/bash.png new file:   img/bitbucket.png new file:   img/csharp.png new file:   img/dataverse.png new file:   img/excel.png new file:   img/github.png new file:   img/github2d.png new file:   img/mysql.png new file:   img/office365.png new file:   img/pipelines.png new file:   img/powerapps.png new file:   img/powerautomate.png new file:   img/powerbi.png new file:   img/powerbiNEW.png new file:   img/powerquery.png new file:   img/powershell.png new file:   img/profile_full.jpg new file:   img/python.png new file:   img/saic.png new file:   img/sharepoint.png new file:   img/tsql.png new file:   index.html new file:   javascript/porfolio.js

---
+++ javascript/porfolio.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass('sticky');
+        }
+        else{
+            $('.navbar').removeClass('sticky');
+        }
+
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass('show');
+        }
+        else{
+            $('.scroll-up-btn').removeClass('show');
+        }
+    });
+
+
+    // slide up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+
+
+    // toggle menue/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass('active');
+        $('.menu-btn i').toggleClass('active');
+    });
+
+    // owl carousel script
+    $('.owl-carousel').owlCarousel({
+        margin: 40,
+        loop: true,
+        autoPlayTimeout: 2000,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            }
+        }
+    });
+});
+
+
+
+// typing animation
+var typed = new Typed(".typed",{
+    strings: ["Engineer", "Administrator","Developer","Designer",""],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
