53ae5e55244badedafae5678660832c1560e17a8
HarryJamesGreenblatt <HarryJamesGreenblatt@ServiceCentricSolutions.com>
2023-04-06 13:52:25 -0700

    modified:   javascript/porfolio.js

--- javascript/porfolio.js
+++ javascript/porfolio.js
@@ -14,7 +14,7 @@ function onMyFrameLoad() {
             // Remove the scroll disabling listener (to enable scrolling again)
             window.removeEventListener('scroll', noscroll);
         }, 
-        1000
+        3000
     );
 }
 
